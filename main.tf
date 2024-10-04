756f70ee0899956f7be39c29e9a5319fe133098b
varshahumbe <infovarshahumbe@gmail.com>
2024-10-04 12:28:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ region = "us-east-2"
 resource "aws_instance" "one" {
 ami = "ami-037774efca2da0726"
 instance_type = "t2.micro"
-key_name = "module2"
+key_name = "dockerr"
 vpc_security_group_ids = ["sg-02dc5520d241dfb63"]
 tags = {
 Name = "terraform-server"
