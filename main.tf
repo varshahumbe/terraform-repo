b03af4494544cf06d2371608a58b53df8240f5f6
varshahumbe <infovarshahumbe@gmail.com>
2024-10-06 14:19:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ provider "aws"{
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-09da212cf18033880"
+  ami           = "ami-09b2477d43bc5d0ac"
   instance_type = "t2.micro"
-  key_name = "module2"
-  vpc_security_group_ids = ["sg-02dc5520d241dfb63"]
+  key_name = "swara"
+  vpc_security_group_ids = ["sg-0bf2d309bfd4f4204"]
   tags = {
     Name = "terraform-server"
   }
