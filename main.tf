083ffbcbfc63dbfba0979ff5382f0f3494066ef9
varshahumbe <infovarshahumbe@gmail.com>
2024-10-18 22:26:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws"{
-    region="us-east-2"
+    region="us-west-1"
 }
 
 resource "aws_instance" "one" {
-  ami           = "ami-09da212cf18033880"
+  ami           = "ami-0cf4e1fcfd8494d5b"
   instance_type = "t2.micro"
   key_name = "asha"
-  vpc_security_group_ids = ["sg-02dc5520d241dfb63"]
+  vpc_security_group_ids = ["sg-0bf2d309bfd4f4204"]
   tags = {
     Name = "test-server"
   }
