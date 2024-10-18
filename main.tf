72721365443aa7ac0ae09b71e270a2ccf9b8fcc1
varshahumbe <infovarshahumbe@gmail.com>
2024-10-18 23:04:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,6 @@ provider "aws"{
 resource "aws_instance" "one" {
   ami           = "ami-0cf4e1fcfd8494d5b"
   instance_type = "t2.micro"
-  key_name = "asha"
-  vpc_security_group_ids = ["sg-0bf2d309bfd4f4204"]
   tags = {
     Name = "test-server"
   }
