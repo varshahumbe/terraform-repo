5fcfddef09e97d41a3651d442b7b12171db64632
varshahumbe <infovarshahumbe@gmail.com>
2024-10-18 23:46:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "one" {
   ami           = "ami-0cf4e1fcfd8494d5b"
   instance_type = "t2.micro"
   tags = {
-    Name = "pipline-server"
+    Name = "pipline1-server"
   }
 }
